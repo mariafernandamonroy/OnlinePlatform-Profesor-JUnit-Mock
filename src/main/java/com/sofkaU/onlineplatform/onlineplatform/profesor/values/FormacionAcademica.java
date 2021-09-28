574b9eee1053f919576e2213435d2084d69aba3b
Maria Fernanda Monroy <mafe2218monroy@gmail.com>
2021-09-27 23:01:23 -0500

    Formacion acdemicaOV created

--- src/main/java/com/sofkaU/onlineplatform/onlineplatform/profesor/values/FormacionAcademica.java
+++ src/main/java/com/sofkaU/onlineplatform/onlineplatform/profesor/values/FormacionAcademica.java
@@ -1,4 +1,46 @@
 package com.sofkaU.onlineplatform.onlineplatform.profesor.values;
 
-public class FormacionAcademica {
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.List;
+import java.util.Objects;
+
+public class FormacionAcademica implements ValueObject<FormacionAcademica.Props> {
+    private final String tituloUniversitario;
+    private final List<String> certificadosAcademicos;
+    private final String especialidad;
+
+    public FormacionAcademica(String tituloUniversitario, List<String> certificadosAcademicos, String especialidad) {
+        this.tituloUniversitario = Objects.requireNonNull(tituloUniversitario);
+        this.certificadosAcademicos = Objects.requireNonNull(certificadosAcademicos);
+        this.especialidad = Objects.requireNonNull(especialidad);
+    }
+
+    @Override
+    public Props value() {
+        return new Props() {
+            @Override
+            public String tituloUniversitario() {
+                return tituloUniversitario;
+            }
+
+            @Override
+            public List<String> certificadosAcademicos() {
+                return certificadosAcademicos;
+            }
+
+            @Override
+            public String especialidad() {
+                return especialidad;
+            }
+        };
+    }
+
+
+    public interface Props{
+        String tituloUniversitario();
+        List<String> certificadosAcademicos();
+        String especialidad();
+
+    }
 }
