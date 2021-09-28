c50292c304ad8607d52034702b54b6722780596c
Maria Fernanda Monroy <mafe2218monroy@gmail.com>
2021-09-27 22:43:36 -0500

    ProfesorId created

--- src/main/java/com/sofkaU/onlineplatform/onlineplatform/profesor/values/ProfesorId.java
+++ src/main/java/com/sofkaU/onlineplatform/onlineplatform/profesor/values/ProfesorId.java
@@ -3,4 +3,11 @@ package com.sofkaU.onlineplatform.onlineplatform.profesor.values;
 import co.com.sofka.domain.generic.Identity;
 
 public class ProfesorId extends Identity {
+    private ProfesorId(String id){
+        super(id);
+    }
+
+    public static ProfesorId of(String id){
+        return new ProfesorId(id);
+    }
 }
