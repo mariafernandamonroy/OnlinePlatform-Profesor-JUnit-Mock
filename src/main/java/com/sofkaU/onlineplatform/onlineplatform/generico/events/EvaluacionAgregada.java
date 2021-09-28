ecc6bde0a0b3b9c05c57ba9c4c41c4dca12884de
Maria Fernanda Monroy <mafe2218monroy@gmail.com>
2021-09-28 00:45:52 -0500

    EvaluacionAgregada aggregate event created

--- src/main/java/com/sofkaU/onlineplatform/onlineplatform/generico/events/EvaluacionAgregada.java
+++ src/main/java/com/sofkaU/onlineplatform/onlineplatform/generico/events/EvaluacionAgregada.java
@@ -7,7 +7,32 @@ import com.sofkaU.onlineplatform.onlineplatform.profesor.values.EvaluacionId;
 import com.sofkaU.onlineplatform.onlineplatform.profesor.values.Proyecto;
 
 public class EvaluacionAgregada extends DomainEvent {
+    private final EvaluacionId evaluacionId;
+    private final Cuestionario cuestionario;
+    private final Proyecto proyecto;
+    private final Calificacion calificacion;
+
     public EvaluacionAgregada(EvaluacionId evaluacionId, Cuestionario cuestionario, Proyecto proyecto, Calificacion calificacion) {
         super("sofkaU.onlineplatform.profesor.evaluacioncreada");
+        this.evaluacionId = evaluacionId;
+        this.cuestionario = cuestionario;
+        this.proyecto = proyecto;
+        this.calificacion = calificacion;
+    }
+
+    public EvaluacionId getEvaluacionId() {
+        return evaluacionId;
+    }
+
+    public Cuestionario getCuestionario() {
+        return cuestionario;
+    }
+
+    public Proyecto getProyecto() {
+        return proyecto;
+    }
+
+    public Calificacion getCalificacion() {
+        return calificacion;
     }
 }
