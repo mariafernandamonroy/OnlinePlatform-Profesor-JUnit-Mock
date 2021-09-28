2986eccb280eda905d5bce6976a2b5c13d11320c
Maria Fernanda Monroy <mafe2218monroy@gmail.com>
2021-09-27 22:33:47 -0500

    correo electronico OV created

--- src/main/java/com/sofkaU/onlineplatform/onlineplatform/generico/globalvalues/CorreoElectronico.java
+++ src/main/java/com/sofkaU/onlineplatform/onlineplatform/generico/globalvalues/CorreoElectronico.java
@@ -1,4 +1,18 @@
 package com.sofkaU.onlineplatform.onlineplatform.generico.globalvalues;
 
-public class CorreoElectronico {
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class CorreoElectronico implements ValueObject<String> {
+
+    private final String email;
+
+    public CorreoElectronico(String email) {
+        this.email = Objects.requireNonNull(email);
+    }
+
+    public String value(){
+        return value();
+    }
 }
