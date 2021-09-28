1c5c3ea648c986fb4d1e69b6e89d2917afea73c1
Maria Fernanda Monroy <mafe2218monroy@gmail.com>
2021-09-28 10:27:13 -0500

    Pregunta entity created

--- src/main/java/com/sofkaU/onlineplatform/onlineplatform/estudiante/Pregunta.java
+++ src/main/java/com/sofkaU/onlineplatform/onlineplatform/estudiante/Pregunta.java
@@ -1,5 +1,27 @@
 package com.sofkaU.onlineplatform.onlineplatform.estudiante;
 
 
-public class Pregunta {
+import co.com.sofka.domain.generic.Entity;
+import com.sofkaU.onlineplatform.onlineplatform.estudiante.values.PreguntaId;
+
+import java.util.Objects;
+
+public class Pregunta extends Entity<PreguntaId> {
+
+    private String descripcionPregunta;
+    private int limiteCaracteres;
+
+    public Pregunta(PreguntaId preguntaId, String descripcionPregunta, int limiteCaracteres ) {
+        super(preguntaId);
+        this.descripcionPregunta = descripcionPregunta;
+        this.limiteCaracteres = limiteCaracteres;
+    }
+
+    public void escribirPregunta(String descripcionPregunta){
+        this.descripcionPregunta = Objects.requireNonNull(descripcionPregunta);
+    }
+
+    public void asignarLimiteCaracteres(int limiteCaracteres){
+        this.limiteCaracteres = Objects.requireNonNull(limiteCaracteres);
+    }
 }
