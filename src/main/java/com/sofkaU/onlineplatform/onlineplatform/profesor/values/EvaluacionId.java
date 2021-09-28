346596aff9d8b1362ba0a45fcfc61b38288bdc34
Maria Fernanda Monroy <mafe2218monroy@gmail.com>
2021-09-27 22:54:06 -0500

    Profesor EvaluacionId OV created

--- src/main/java/com/sofkaU/onlineplatform/onlineplatform/profesor/values/EvaluacionId.java
+++ src/main/java/com/sofkaU/onlineplatform/onlineplatform/profesor/values/EvaluacionId.java
@@ -1,4 +1,13 @@
 package com.sofkaU.onlineplatform.onlineplatform.profesor.values;
 
-public class EvaluacionId {
+import co.com.sofka.domain.generic.Identity;
+
+public class EvaluacionId extends Identity {
+    private EvaluacionId(String id){
+        super(id);
+    }
+
+    public static EvaluacionId of(String id){
+        return new EvaluacionId(id);
+    }
 }
