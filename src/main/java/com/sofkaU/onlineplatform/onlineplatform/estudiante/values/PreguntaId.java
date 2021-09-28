966c6f84f1f12f129feed6ff902cb523c928e28d
Maria Fernanda Monroy <mafe2218monroy@gmail.com>
2021-09-28 09:47:58 -0500

    PreguntaId VO created

--- src/main/java/com/sofkaU/onlineplatform/onlineplatform/estudiante/values/PreguntaId.java
+++ src/main/java/com/sofkaU/onlineplatform/onlineplatform/estudiante/values/PreguntaId.java
@@ -2,5 +2,12 @@ package com.sofkaU.onlineplatform.onlineplatform.estudiante.values;
 
 import co.com.sofka.domain.generic.Identity;
 
-public class PreguntaId implements Identity {
+public class PreguntaId extends Identity {
+    private PreguntaId(String id){
+        super(id);
+    }
+
+    public static PreguntaId of(String id){
+        return new PreguntaId(id);
+    }
 }
