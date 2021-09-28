7ef1256526687b585945d8e8f86c718b67f7bb1a
Maria Fernanda Monroy <mafe2218monroy@gmail.com>
2021-09-28 00:53:22 -0500

    CalificacionProyectoEvaluacionCalculada aggregate event created

---
+++ src/main/java/com/sofkaU/onlineplatform/onlineplatform/generico/events/calificacionProyectoEvaluacionCalculada.java
@@ -0,0 +1,31 @@
+package com.sofkaU.onlineplatform.onlineplatform.generico.events;
+
+import co.com.sofka.domain.generic.DomainEvent;
+import com.sofkaU.onlineplatform.onlineplatform.profesor.values.Calificacion;
+import com.sofkaU.onlineplatform.onlineplatform.profesor.values.EvaluacionId;
+import com.sofkaU.onlineplatform.onlineplatform.profesor.values.Proyecto;
+
+public class calificacionProyectoEvaluacionCalculada extends DomainEvent {
+    private EvaluacionId evaluacionId;
+    private Proyecto proyecto;
+    private Calificacion calificacion;
+
+    public calificacionProyectoEvaluacionCalculada(EvaluacionId evaluacionId, Proyecto proyecto, Calificacion calificacion) {
+        super("sofkaU.onlineplatform.profesor.calificacionproyectoevaluacioncalculada");
+        this.evaluacionId = evaluacionId;
+        this.proyecto = proyecto;
+        this.calificacion = calificacion;
+    }
+
+    public EvaluacionId getEvaluacionId() {
+        return evaluacionId;
+    }
+
+    public Proyecto getProyecto() {
+        return proyecto;
+    }
+
+    public Calificacion getCalificacion() {
+        return calificacion;
+    }
+}
