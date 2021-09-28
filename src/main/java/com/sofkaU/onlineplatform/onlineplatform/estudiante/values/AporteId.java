8ab5f6c6acb81f4e9f6adf584a9c5163be2f3f73
Maria Fernanda Monroy <mafe2218monroy@gmail.com>
2021-09-28 09:47:03 -0500

    AporteId VO created

--- src/main/java/com/sofkaU/onlineplatform/onlineplatform/estudiante/values/AporteId.java
+++ src/main/java/com/sofkaU/onlineplatform/onlineplatform/estudiante/values/AporteId.java
@@ -2,5 +2,13 @@ package com.sofkaU.onlineplatform.onlineplatform.estudiante.values;
 
 import co.com.sofka.domain.generic.Identity;
 
-public class AporteId implements Identity {
+public class AporteId extends Identity {
+    private AporteId(String id){
+        super(id);
+    }
+
+    public static AporteId of(String id){
+        return new AporteId(id);
+    }
+
 }
